61277e49f524954452721dbc61a5f3f6f7f0e7d7
ciaoradwen <ciaoradwen@gmail.com>
2019-12-07 14:48:30 +0100

    firebase

--- js/main.js
+++ js/main.js
@@ -75,3 +75,47 @@ $('#bouton8').on('click', function () {
     $('#Track8').modal();
     
 })
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAMGTMb0PZX6nh9e9RUYad3otUXl8AylVA",
+    authDomain: "web1-e7b42.firebaseapp.com",
+    databaseURL: "https://web1-e7b42.firebaseio.com",
+    projectId: "web1-e7b42",
+    storageBucket: "web1-e7b42.appspot.com",
+    messagingSenderId: "1004232803428",
+    appId: "1:1004232803428:web:deb4f2f440833e2fce8b71",
+    measurementId: "G-5RL5G48HH5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  console.log(firebase)
+  
+//save message to firebase
+  var contactsRef = firebase.database().ref('contactsDatabase');// setting the name of yourdata base//
+  function saveContact (name, track, email, phone){
+      var newContactRef = contactsRef.push();
+      newContactRef.set({
+          name: name,
+          track: track,
+          email: email,
+          phone: phone,
+      })
+  }
+
+  function submitForm(event){
+      event.preventDefault();
+      //get values
+      var name = document.getElementById('name').value;
+      var track = document.getElementById('track').value;
+      var email= document.getElementById('email').value;
+      var phone =  document.getElementById('phone').value;
+      console.log(name, track)
+     
+      saveContact(name, track, email, phone);//sending data to our database
+    document.getElementById('contactForm').reset();
+    }
+
+  document.getElementById('contactForm').addEventListener('submit',submitForm);
+
+  
+  
